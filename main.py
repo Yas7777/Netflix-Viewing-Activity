62805daf6135acfb839d623c355c6d272a52c852
Yas7777 <yasmeen.azeem@hotmail.com>
2022-05-24 23:41:54 -0700

    :)

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import math
 from matplotlib import pyplot
 import pytz
 from datetime import datetime, timezone
+import datetime
+sns.set()
+
+# how many minutes of TV I have watched - line chart?
+# % of movies vs TV shows I have watched - stacked bar
+# Top 10 Binge-Watching TV Series - bar graph
+# HeatMap of my Viewing Activity of one show - Friends
+
 
 # Input the netflix file here
 netflix = '/Users/yasmeen/Desktop/ViewingActivity.csv'
@@ -24,7 +32,7 @@ df.drop("Bookmark", axis=1, inplace=True)
 df.drop("Latest Bookmark", axis=1, inplace=True)
 df.drop("Attributes", axis=1, inplace=True)
 
-# Supplemental Video Type - only
+# TODO:Supplemental Video Type - only
 df = df[df['Supplemental Video Type'].isna()]
 
 # graph of viewing activity
@@ -35,34 +43,26 @@ plt.ylabel("Freq", fontsize=14)
 plt.xlabel("Profile Names", fontsize=14)
 plt.xticks(fontsize=11)
 plt.title("Viewing Frequency", fontsize=16)
-plt.show()
 
 
-#how_details = df.Title.str.split(":", expand=True, n=2)
 
-# show_detail
-#df['show_name'] = show_details[0]
-#df['season'] = show_details[1]
-#df['episode_name'] = show_details[2]
-
-# If the season column is "None" them it is most likely a movie, lets add another column to our dataframe
-# my_history[my_history['season'].isna()]
-#df['show_type'] = df.apply(lambda x:'Movie' if pd.isnull(x['season']) else 'TV Show', axis=1)
 
 
 # make a new dataframe'
-#dfl = df[df['Profile Name'] == 'Yasmeen']
+dfl = df[df['Profile Name'] == 'Yasmeen']
 
-"""x = dfl.groupby(['Type'])['Type'].count()
+show_details = df.Title.str.split(":", expand=True, n=2)
+# show_detail
+dfl['Show name'] = show_details[0]
+dfl['Season'] = show_details[1]
+dfl['Episode Name'] = show_details[2]
+
+# If the season column is "None" them it is most likely a movie, lets add another column to our dataframe
+# my_history[my_history['season'].isna()]
+dfl['Show Type'] = dfl.apply(lambda x:'Movie' if pd.isnull(x['Season']) else 'TV Show', axis=1)
+
+
+x = dfl.groupby(['Show Type'])['Show Type'].count()
 y = len(dfl)
 r = ((x/y)).round(2)
 mf_ratio = pd.DataFrame(r).T
-print(dfl)"""
-
-"""pyplot.xlabel("Profile")
-pyplot.ylabel("Duration in minutes")
-pyplot.plot(df['Profile Name'], df['duration_minutes'] , 'ro--', linewidth=2, label='Bubble Sort')
-
-pyplot.legend(loc='upper left')
-pyplot.show()"""
-
