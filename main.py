949a036af5a4f5b89bb15660ad15bb5e63e17014
Yas7777 <yasmeen.azeem@hotmail.com>
2022-05-24 21:08:59 -0700

    :)

--- main.py
+++ main.py
@@ -3,54 +3,68 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
+from matplotlib import pyplot
+import pytz
+from datetime import datetime, timezone
 
-# netflix file
+# Input the netflix file here
 netflix = '/Users/yasmeen/Desktop/ViewingActivity.csv'
 # read the file
 df = pd.read_csv(netflix)
-# remove Supplemental Video Type coloumn
-del df['Supplemental Video Type']
-columns_to_drop = ["Country", "Bookmark", "Latest Bookmark", "index", "Attributes", "Supplemental Video Type"]
-my_data = df.drop(columns = columns_to_drop)
 
-show_details = df.Title.str.split(":",expand=True,n=2)
+# Convert duration HH:MM:SS to number of minutes
+df['duration_minutes'] = pd.to_timedelta(df['Duration']).dt.total_seconds()/60
+
+# only include viewings with at least 15 minutes duration
+df = df[df['duration_minutes'] >= 15]
+
+# remove columns that mean nothing
+df.drop("Bookmark", axis=1, inplace=True)
+df.drop("Latest Bookmark", axis=1, inplace=True)
+df.drop("Attributes", axis=1, inplace=True)
+
+# Supplemental Video Type - only
+df = df[df['Supplemental Video Type'].isna()]
+
+# graph of viewing activity
+profile_count = df["Profile Name"].value_counts()
+plt.figure(figsize=(8,5))
+plt.bar(profile_count.index, profile_count.values, color="pink")
+plt.ylabel("Freq", fontsize=14)
+plt.xlabel("Profile Names", fontsize=14)
+plt.xticks(fontsize=11)
+plt.title("Viewing Frequency of Each Profile", fontsize=16)
+plt.show()
+#columns_to_drop = ["Country", "Bookmark", "Latest Bookmark", "index", "Attributes", "Supplemental Video Type"]
+#my_data = df.drop(columns_to_drop)
+
+#how_details = df.Title.str.split(":", expand=True, n=2)
+
 # show_detail
-df['show_name'] = show_details[0]
-df['season'] = show_details[1]
-df['episode_name'] = show_details[2]
+#df['show_name'] = show_details[0]
+#df['season'] = show_details[1]
+#df['episode_name'] = show_details[2]
 
 # If the season column is "None" them it is most likely a movie, lets add another column to our dataframe
 # my_history[my_history['season'].isna()]
-df['show_type'] = df.apply(lambda x:'Movie' if pd.isnull(x['season']) else 'TV Show', axis=1)
+#df['show_type'] = df.apply(lambda x:'Movie' if pd.isnull(x['season']) else 'TV Show', axis=1)
 
-# 0:01:05 -> 65
-def durationTimeToSeconds(duration):
-    try:
-        [hour, minutes, seconds] = duration.split(':')
-        return int(hour)*3600 + int(minutes)*60 + int(seconds)
-    except:
-        return 0
 
-# 65 -> 00:01::05
-def secondsToDurantion(seconds):
-    hours = math.floor(seconds/3600)
-    remainingSeconds = seconds - (hours*3600)
-    minutes = math.floor(remainingSeconds/60)
-    remainingSeconds = remainingSeconds - (minutes*60)
-    return '{:02d}:{:02d}:{:02d}'.format(hours, minutes, remainingSeconds)
-'''
-# duration = "00:21:12"
-# Convert to minutes = first split the duration into a list by removing ":"
-df['duration_minutes'] = df['Duration'].str.split(':')
-# Convert to minutes = second i[0]*60 + i[1]
-df['duration_minutes'] = df['duration_minutes'].apply(lambda x: int(x[0]) * 60 + int(x[1]))
-# filter duration time to only more than 10
-df = df[df['duration_minutes'] >= 10]'''
-
+"""df = df[df['Supplemental Video Type'].isna()]"""
 # make a new dataframe'
-dfl = df[df['Profile Name'] == 'Yasmeen']
+#dfl = df[df['Profile Name'] == 'Yasmeen']
 
-x = dfl.groupby(['Type'])['Type'].count()
+"""x = dfl.groupby(['Type'])['Type'].count()
 y = len(dfl)
 r = ((x/y)).round(2)
 mf_ratio = pd.DataFrame(r).T
+print(dfl)"""
+
+"""pyplot.xlabel("Profile")
+pyplot.ylabel("Duration in minutes")
+pyplot.plot(df['Profile Name'], df['duration_minutes'] , 'ro--', linewidth=2, label='Bubble Sort')
+
+pyplot.legend(loc='upper left')
+pyplot.show()"""
+
