ac541dc499b22e58959bceb9398db17cc408a092
Yas7777 <yasmeen.azeem@hotmail.com>
2022-06-13 17:44:07 -0700

    :)

---
+++ Readme.md
@@ -0,0 +1,51 @@
+**My Netflix Viewing Activity Visualization Project**
+
+I was interested to see my Netflix activity since I have been using Netflix for over six years. 
+The entire project used libraries that included pandas, matplotlib, and seaborn. Python 3 was the programming language used for this project.
+
+1 – Grouped Bar Graph by Profile
+
+* **Details :** Duration (in minutes) that each profile in my account has watched over the years.Illustrates how much each Netflix account watched (in terms of duration minutes) from 2016 to 2021. 
+* **Image:**
+* **Parameters:**  
+  * X-Axis: Shows years 2016 through 2021. 2015 and 2022 are not included as the data for those years were incomplete, and I wanted the graphs to portray a complete data set. 
+  * Y-Axis: Shows duration in minutes.
+* **Parameters:**  My viewing frequency is significantly higher than the other profile. This is because TV watching is a hobby and relaxation activity for me. I like keeping up with new shows as they come out. The "binge" factor of shows has also severely contributed to my high duration.
+
+2 – Stacked Bar Graph:
+* **Details:** TV Shows watched vs Movies in terms of Duration (minutes) each year.
+* **Image:**
+* **Parameters:**
+  * X-Axis: Shows years 2016 through 2021. 2015 and 2022 are not included as the data for those years were incomplete, and I wanted the graphs to portray a complete data set. 
+  * Y-Axis: Shows duration in minutes – the data can also be changed to portray in hours.
+* **Analysis**:  Netflix's movie collection slowly started shifting to original movie content which I found boring. I don't particularly appreciate wasting time on lousy entertainment when many other options are available. I also enjoy the episodic nature of TV shows more.
+
+3 - Horizontal Bar Graph
+* **Details:** Top 10 TV shows watched in terms of Duration (minutes)
+* **Image:**
+* **Parameters:**
+  * X-Axis: Shows duration in minutes. 
+  * Y-Axis: TV shows
+* **Analysis**:  Friends is a comfort show that I have been a loyal fan of for a long time. This is a show I played on repeat, hence the high duration in this graph.
+
+4- Heatmap 
+* **Details**: Heatmap of my viewing activity of Friends
+* **Image:**
+* **Parameters:**
+  * X-Axis: Years (does not contain data for 2020 as Friends left the Netflix USA platform)
+  * Y-Axis: Months
+* **Analysis:** Given that Graph 3 pointed out that Friends was the most-watched show, I wanted to understand how much time I spent watching that show over the years it was on Netflix over the years and which month was my most highly watched. 
+
+**Programming Language:**  Python 3
+
+**Source Data:** Personal Netflix Activity downloaded into a csv format through www.netflix.com
+
+**My Hot Takes:** 
+* The CSV file from Netflix requires significant clean-up to make it usable. 
+* I did not use seaborn for stacked bar graphs as I found it particularly hard to use. 
+* With every graph created, many sanity checks are needed to ensure that the data is correct. Some are included in this code, however, I have significantly condensed it here for readability and to ensure the focus is on the graphs.
+
+**Future Improvements:** 
+•	Include the IMDb library to see how ratings correlate with my viewing activity.
+•	Include my viewing activity data from other streaming platforms' data (HBO, Hulu, etc.)
+•	Streamline data clean-up such that the code can be used for any user's data without amending the original code to account for that user's specific Netflix viewing activity.
--- main.py
+++ main.py
@@ -1,53 +1,58 @@
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import math
-import matplotlib
-import pytz
-from datetime import datetime, timezone, date
-import datetime
-
 # Setting seaborn as default style even if only matplotlib is used
 sns.set(style="white")
 
-# Input the netflix file here
+# Input the Netflix file here
 netflix = '/Users/yasmeen/Desktop/ViewingActivity.csv'
-# Read the file and save into a dataframe
+# Read the file and save into a dataframe (called "DF" from here on out)
 df = pd.read_csv(netflix)
 
-# Clean - Up
+# Data Clean - Up
 
-# Pandas dataframe.isna() function is used to detect missing values. This removes those values
+# Supplemental Video Type consists of values such as "hook", "trailer" etc which we do not want to include in the data
+# analysis. We only want feature length TV Shows/Movies in our DF which has no value. Pandas dataframe.isna() function
+# is used to detect missing values. The code below ensure that the DF only consists of data with the missing values i.e
+# only consists of movies and tv shows.
+
 df = df[df['Supplemental Video Type'].isna()]
 # Convert default duration HH:MM:SS to number of minutes
 df['duration_minutes'] = pd.to_timedelta(df['Duration']).dt.total_seconds()/60
 # Only include viewings with at least 15 minutes duration
 df = df[df['duration_minutes'] >= 15]
-# Remove columns that will not be used for this project
+# Remove columns that will not be used for this data analysis.
 df.drop("Bookmark", axis=1, inplace=True)
 df.drop("Latest Bookmark", axis=1, inplace=True)
 df.drop("Attributes", axis=1, inplace=True)
 
-show_details = df.Title.str.split(":", expand=True)#, n=2)
-# show_detail split into Show Name, Season, Episode Name
-df['show_name'] = show_details[0]
+# Show Details needs to be split up into Show name, Season and Episode Name. Currently, in one string. This code below
+# splits it into 3 columns
+show_details = df.Title.str.split(":", expand=True, n=2)
+# show_detail split into Show Name, Season, Episode Name (Column 0, 1, and 2)
+df['Show Name'] = show_details[0]
 df['Season'] = show_details[1]
 df['Episode Name'] = show_details[2]
 
-# If the season column is "None" them it is most likely a movie, lets add another column to our dataframe
-# my_history[my_history['season'].isna()]
+
+# If the season column is "None" -> it is a movie, lets add another column to the DF
 df['Show Type'] = df.apply(lambda x:'Movie' if pd.isnull(x['Season']) else 'TV Show', axis=1)
-# Graph 1 - How many minutes have the profiles watched?
-# Total duration minutes summed across all years
-profile_count = df["Profile Name"]
-duration_minutes = df['duration_minutes']
+# Convert Start Time to datetime. Currently, it is an object.
+
 df['Start Time'] = pd.to_datetime(df['Start Time'])
+# We need the years and months
 df['Year'] = df['Start Time'].dt.year
 df['Month'] = df['Start Time'].dt.month
 # Only use years that are not = 2015 and 2022
 df = df[df['Year'] != 2015]
 df = df[df['Year'] != 2022]
+
+# Graph 1 - How many minutes have the profiles watched?
+
+# Total duration minutes summed across all years
+profile_count = df["Profile Name"]
+duration_minutes = df['duration_minutes']
+
 # Count the Years
 year = df['Year']
 
@@ -64,31 +69,21 @@ chart = sns.barplot(
     )
 # sum = df['duration_minutes'].sum()
 # print(sum)
+
 # Sets the format of Graph 1
 plt.title("Viewing Frequency", fontsize=14)
-plt.xlabel("Years")
-plt.ylabel("Number of Minutes")
+plt.xlabel("")
+plt.ylabel("Duration (in Minutes)")
 # Add labels to the bar chart
 for container in chart.containers:
-    chart.bar_label(container, size=8)
+    chart.bar_label(container, size=10, fmt='%0.0f')
 chart.margins(y=0.1)
 plt.tight_layout()
 plt.show()
 
-"""TODO: # Further cleanup - Delete
 
-show_details = df.Title.str.split(":", expand=True)#, n=2)
-# show_detail split into Show Name, Season, Episode Name
-df['show_name'] = show_details[0]
-df['Season'] = show_details[1]
-df['Episode Name'] = show_details[2]
+# Graph 2 - Movies vs TV shows
 
-# If the season column is "None" them it is most likely a movie, lets add another column to our dataframe
-# my_history[my_history['season'].isna()]
-df['Show Type'] = df.apply(lambda x:'Movie' if pd.isnull(x['Season']) else 'TV Show', axis=1)"""
-
-
-# % of movies vs TV shows I have watched - stacked bar
 # only use my Profile
 df = df[df['Profile Name'] == "Yasmeen"]
 # Sanity - Check
@@ -102,23 +97,21 @@ show_type = df.groupby(['Year', 'Show Type'])['duration_minutes'].sum().unstack(
 plt.figure(figsize=(10, 6))
 plt.xticks(rotation=0, ha='center')
 
+# Plot graph
 # First plot the 'TV Show' bars
 fig, ax = plt.subplots(1)
-ax.bar(show_type.index, show_type['TV Show'], label='TV Show', color='pink')
+ax.bar(show_type.index, show_type['TV Show'], label='TV Show', color='#d4b9da')
 # Plot the 'Movie' bars on top, starting at the top of the 'TV Show' bars.
 ax.bar(show_type.index, show_type['Movie'], bottom=show_type['TV Show'],
-       label='Movie', color="red")
+       label='Movie', color= '#df65b0')
 ax.legend()
 
 for c in ax.containers:
-    # Optional: if the segment is small or 0, customize the labels
-    labels = [v.get_height() if v.get_height() > 0 else '' for v in c]
-    # remove the labels parameter if it's not needed for customized labels
-    ax.bar_label(c, fmt='%0.0f', label_type='center')
+    # Adding labels to the graph
+    ax.bar_label(c, size=10, fmt='%0.0f', label_type='center')
 
-ax.set_title('TV Shows vs Movies')
-# Remove 'Count' ylabel.
-ax.set_ylabel(None)
+ax.set_title("Yasmeen's Viewing Frequency - TV Shows vs Movies")
+ax.set_ylabel("Duration (in minutes)")
 plt.tight_layout()
 
 # deleting the empty plot for now
@@ -127,49 +120,42 @@ plt.show()
 
 # Graph 3- Vertical Stacked Graph -  Most watched TV Shows
 
-tv_show_groupby = df.groupby('show_name')['duration_minutes'].sum().reset_index().sort_values(by='duration_minutes',
-                                                                                             ascending=False)
-# tv_show_groupby['duration_hours'] = TVshow_groupby['duration_minutes']/60
+tv_show_groupby = df.groupby('Show Name')['duration_minutes'].sum().reset_index().sort_values(by='duration_minutes',
+                                                                                              ascending=False)
 # setting the size and style of chart
 plt.figure(figsize=(10, 6))
 
+# plot graph
 ax = sns.barplot(
     x=tv_show_groupby['duration_minutes'][:10],
-    y=tv_show_groupby['show_name'][:10],
+    y=tv_show_groupby['Show Name'][:10],
     palette="PuRd",
     ci=None)
 
 # Adding labels to the vertical stacked graph
 for c in ax.containers:
-    # remove the labels parameter if it's not needed for customized labels
     ax.bar_label(c, fmt='%0.0f', label_type='edge')
 
-plt.title("Top 10 TV Shows by Duration")
-plt.xlabel("")
-plt.ylabel("Frequency of Watching")
+plt.title("Top 10 TV Shows (by duration)")
+plt.xlabel("Duration (in minutes)")
+plt.ylabel("")
+plt.tight_layout()
 plt.show()
 
 
-# heatmap
-# lets work on a new data frame that is copy paste this frame
-# we need the years and months
+# Graph 4- Heatmap - Showing Activity on watching "Friends"
 
-# Convert Start Time to datetime. Currently it is an object
-df['Start Time'] = pd.to_datetime(df['Start Time'])
-df['Year'] = df['Start Time'].dt.year
-df['Month'] = df['Start Time'].dt.month
-# df['Day of Week'] = df['Start Time'].dt.dayofweek
-# df['Day'] = df['Start Time'].dt.day_name()
-df = df[df['show_name'] == 'Friends']
+# We only want to look at "Friends" for this heatmap
+df = df[df['Show Name'] == 'Friends']
+# did a check to see if my DF had any  if there is data for 2020.
 df = df[df['Year'] != 2021]
-# create a copy of the dataframe, and add columns for month and year
+# Using a new DF for the Heatmap, create a copy of the DF
 df_m = df.copy()
 # group by month and year, get the sum
 df_m = df_m.groupby(['Year', 'Month']).sum()
 df_m = df_m.unstack(level=0)
 
-# pd.set_option("display.max_rows", None, "display.max_columns", None)
-# fig, ax = plt.subplots(figsize=(11, 9))
+
 # plot heatmap
 x_axis_labels = [2016, 2017, 2018, 2019]
 sns.heatmap(df_m,
@@ -179,9 +165,13 @@ sns.heatmap(df_m,
             vmin=25,
             vmax=2700,
             linewidth=0.3,
-            cbar_kws={"shrink": .8},
-            annot_kws={"size": 7},
-            xticklabels = x_axis_labels
+            cbar_kws={'label': 'Duration (in minutes)'},
+            annot_kws={"size": 10},
+            xticklabels=x_axis_labels
             )
 
+plt.title("Friends TV Show - Heatmap")
+plt.xlabel("")
+plt.ylabel("Month")
+plt.tight_layout()
 plt.show()
